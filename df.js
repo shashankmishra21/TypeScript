71f3471db1b140f7b8b03a97c002bbb44702a473
shashankmishra21 <mishrashashank2106@gmail.com>
2025-06-06 23:59:25 +0530

    update typescript

---
+++ df.js
@@ -0,0 +1,9 @@
+"use strict";
+function isLegal(user) {
+    if (user.age > 18) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
---
+++ df.ts
@@ -0,0 +1,14 @@
+interface User {
+	firstName: string;
+	lastName: string;
+	email: string;
+	age: number;
+}
+
+function isLegal(user: User) {
+    if (user.age > 18) {
+        return true
+    } else {
+        return false;
+    }
+}
